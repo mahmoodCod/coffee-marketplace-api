ed2b4614a0afa951dc12a3ac2b43d8096d511e30
mahmoodCod <nmashoomy11@gmail.com>
2026-09-03 16:56:07 +0400

    feat(coupons): connect orders module
    - import OrdersModule into CouponModule
    - enable OrderRepository injection in CouponService

--- coffee-marketplace-api/src/modules/coupons/coupon.module.ts
+++ coffee-marketplace-api/src/modules/coupons/coupon.module.ts
@@ -4,9 +4,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coupon } from './entities/coupon.entity';
 import { CouponRepository } from './repositories/coupon.repository';
 import { CouponService } from './services/coupon.service';
+import { CouponController } from './controllers/coupon.controller';
+import { OrdersModule } from '../orders/order.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Coupon])],
+  imports: [TypeOrmModule.forFeature([Coupon]), OrdersModule],
+  controllers: [CouponController],
   providers: [CouponRepository, CouponService],
   exports: [CouponRepository, CouponService],
 })
